6b6d2394667e6b06b6918a31053420fbb559beaf
MalvinMM <juniormalvinn@gmail.com>
2024-04-17 08:52:11 +0700

    Update - Adding red asterisk for db view, function, and store procedure form

--- resources/views/admin/erp/db_view/add_view.blade.php
+++ resources/views/admin/erp/db_view/add_view.blade.php
@@ -15,21 +15,22 @@
             <form action="{{ route('storeView', $erp) }}" method="POST">
                 @csrf
                 <div class="mb-3">
-                    <label for="Name" class="form-label">Nama Database View</label>
+                    <label for="Name" class="form-label">Nama Database View <span style="color: red;">*</span></label>
                     <input type="text" class="form-control" id="Name" name="Name" value="{{ old('Name') }}">
                     @error('Name')
                         <h6 class="form-helper"style="color:red">{{ $message }}</h6>
                     @enderror
                 </div>
                 <div class="mb-3">
-                    <label for="Description" class="form-label">Deskripsi Database View</label>
+                    <label for="Description" class="form-label">Deskripsi Database View <span
+                            style="color: red;">*</span></label>
                     <textarea class="form-control" id="Description" name="Description" rows="10">{{ old('Description') }}</textarea>
                     @error('Description')
                         <h6 class="form-helper" style="color:red">{{ $message }}</h6>
                     @enderror
                 </div>
                 <div class="mb-3">
-                    <label for="SQL_Query" class="form-label">SQL Query</label>
+                    <label for="SQL_Query" class="form-label">SQL Query <span style="color: red;">*</span></label>
                     <textarea class="form-control" id="SQL_Query" name="SQL_Query" rows="15">{{ old('SQL_Query') }}</textarea>
                     @error('SQL_Query')
                         <h6 class="form-helper" style="color:red">{{ $message }}</h6>
